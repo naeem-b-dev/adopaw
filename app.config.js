7d6271aaa787a9b644951ea3dec963a1189e4d84
mimic00-cpu <naimb2798@gmail.com>
2025-08-18 15:18:48 +0300

    map_v5

--- app.config.js
+++ app.config.js
@@ -1,6 +1,13 @@
 require("dotenv").config();
 
 module.exports = ({ config }) => {
+  const apiKey = process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY;
+  if (!apiKey) {
+    // Leave a visible hint in console during prebuild, so we don't ship an empty key
+     
+    console.warn("[app.config] Missing EXPO_PUBLIC_GOOGLE_MAPS_API_KEY in .env");
+  }
+
   return {
     ...config,
     android: {
@@ -8,7 +15,7 @@ module.exports = ({ config }) => {
       config: {
         ...((config.android && config.android.config) || {}),
         googleMaps: {
-          apiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY || ""
+          apiKey: apiKey || ""
         }
       }
     }
